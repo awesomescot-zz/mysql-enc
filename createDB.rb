7700a3465ab9ddbf8741223a7d17ea73aaaa0d83
Ubuntu <ubuntu@dev-puppet.datareadings.com>
2013-04-22 23:29:54 +0000

    adding more files

--- createDB.rb
+++ createDB.rb
@@ -1,8 +1,19 @@
-require mysql
+require "rubygems"
+#gem "mysql2", "~> 0.2.7"
+require "mysql2"
 
-my = Mysql::new('localhost', 'user', 'passwd', 'db')
-res = my.query("select * from tbl")
-res.each do |row|
-  col1 = row[0]
-  col2 = row[1]
+@db_host = "localhost"
+@db_user = "root"
+@db_password = "puppet"
+@db_name = "enc"
+
+client = Mysql2::Client.new(:host => @db_host, :username => @db_user, :password => @db_password)
+
+client.query("DROP DATABASE IF EXISTS #{@db_name}")
+client.query("CREATE DATABASE #{@db_name}")
+client.query("show databases").each(:as => :array) do |row|
+  puts row
 end
+client.query("grant all on enc.* to '%'@'localhost';")
+client.query("use enc")
+client.query("CREATE TABLE nodes ( id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, nodename VARCHAR(100), classes VARCHAR(200), created TIMESTAMP DEFAULT NOW() )")
